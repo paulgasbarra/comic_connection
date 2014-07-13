6bfc35e28c5e51aaddebf96eb31c511d286e6276
paulgasbarra <paulgasbarra@gmail.com>
2014-07-13 12:15:19 -0400

    sunday morning commit

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -18,6 +18,7 @@ $(function(){
   $('.submit').on('click', function (e){
 
     e.preventDefault();
+    console.log('you clicked?')
     $("#characterData").html("")
     $("<p>").html('Loading...').appendTo("#characterData")
     var api = 'http://www.comicvine.com/api/characters';
@@ -31,7 +32,6 @@ $(function(){
     var requri =  api + api_key + filter + character + limit + format + fields;
 
     console.log(requri);
-
 
 
     $.ajax({
@@ -49,18 +49,44 @@ $(function(){
             name = feed[index].name
             issues = feed[index].count_of_issue_appearances
             id = feed[index].id
-            idFilter = "&filter=id:"
-            heroLink = "<a href='"+ api+ idFilter + id + api_key+"'>" + name + " " + issues+"</a>"
-            //http://www.comicvine.com/api/character/4005-3628/
-            $("<li>").html(heroLink).appendTo("#characterData")
+            // characterAPI = 'http://www.comicvine.com/api/character'
+            // resource = "/4005-"
+            // heroFields = "&field_list=name,id,count_of_issue_appearances,image,deck,character_enemies,character_friends,powers,teams"
+            // heroLink = "<a href='"+ characterAPI + resource + id + api_key+ format + heroFields +"'>" + name + " " + issues+"</a>"
+            //heroLink = "<a href='#'>" + name + " " + issues +"</a>"
+            $link = $("<li>").addClass("hero")
+            $link.attr('value', id)
+            $link.html(heroLink).appendTo("#characterData")
+          })//feed.each loop
+        }//end success function
+    })//ajax call
+  })//onclick function
 
-          })
+  $('.submit').on('click', function (e){
+     e.preventDefault();
+     console.log("You clicked me!")
+     $("#characterStats").html("")
 
-        }
-    })
 
-  })
-});
+    var api = 'http://www.comicvine.com/api/character';
+    var api_key = "/?api_key=c449b6dfe0d7bc76f14627f06f9ba2b2adb532b5";
+    var format = "&format=json"
+    var resource = "/4005-"
+    var id = this.value
+    var heroFields = "&field_list=name,id,count_of_issue_appearances,image,deck,character_enemies,character_friends,powers,teams"
+    var requri = api + api_key + format + resource + id + heroFields
+    console.log(requri)
+    $.ajax({
+        type: 'get',
+        url: '/comicchar',
+        data: {url: requri},
+        dataType: 'json',
+        success: function(data){
+          console.log(data)
+        }//end success fnction
+    })//ajax call
+  })//heroClicks
+});//onload function
 
 
 
